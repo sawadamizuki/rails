e6d6bdb166c8d14d2f451c4f8c7b5b3e17c1e594
EC2 Default User <ec2-user@ip-172-31-37-137.ec2.internal>
2021-12-17 03:21:42 +0000

    [add]画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a04e2ef6a117e9893ce33bcaece1ebb3b3fdef5a1182c50d15990de8de2c4fd778cb1248d5534d0acf02ad7a706f64479dc2ab5383455e3dc4e3e37b516cab68'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_054745) do
+ActiveRecord::Schema.define(version: 2021_12_17_025933) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
